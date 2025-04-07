913d325b34b763daf1a7715a9f647ade4be7765c
loubna-gout <loubnagout7@gmail.com>
2025-04-07 23:48:14 +0200

    Update exercice2.js

--- exercice2.js
+++ exercice2.js
@@ -1,24 +1,15 @@
-// Générer un nombre aléatoire entre 1 et 10
 const nombreChoiser = Math.floor(Math.random() * 10) + 1;
 let itération = 0;
 let trouve = false;
-
 alert("Devinez le nombre entre 1 et 10 !");
 
 while (!trouve) {
-    // Demander à l'utilisateur de deviner
-    const proposition = prompt("Entrez votre proposition (entre 1 et 10):");
-    
-    // Vérifier si l'utilisateur a annulé
+  const proposition = prompt("Entrez votre proposition (entre 1 et 10):");
     if (proposition === null) {
         alert("Vous avez annulé le jeu. Le nombre était " + nombreChoiser);
         break;
     }
-    
-    // Convertir en nombre
     const nombrePropose = parseInt(proposition);
-    
-    // Vérifier si c'est un nombre valide
     if (isNaN(nombrePropose)) {
         alert("Veuillez entrer un nombre valide !");
         continue;
@@ -28,11 +19,9 @@ while (!trouve) {
         alert("Le nombre doit être entre 1 et 10 !");
         continue;
     }
-    
-    // Incrémenter le compteur de tentatives
+
     itération++;
     
-    // Vérifier la proposition
     if (nombrePropose === nombreChoiser) {
         trouve = true;
         alert("Bravo ! Vous avez trouvé le nombre " + nombreChoiser + " en " + itération+ " tentative(s).");
